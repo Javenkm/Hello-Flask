796948d6bb08d2323de305fe72acc825d0d2adca
Javenkm <Javenkm@gmail.com>
2021-05-24 19:59:47 -0700

    init commit

---
+++ hello.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+app = Flask (__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', phrase = "hello", times = 5)
+
+
+@app.route('/name')
+def hello_person(name):
+    print("*"*80)
+    print("in hello_person function")
+    print(name)
+    return f"Hello {name}!"
+
+
+@app.route('/success')
+def success():
+    return "success"
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    print (name)
+    return "Hello, " + name
+
+
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
